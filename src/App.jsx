2718b2a23614d600c4c3e81779cc49dff6119abb
himu <satirtharoy2003@gmail.com>
2024-05-22 17:43:51 +0600

    scroll based animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { motion, useInView } from "framer-motion"
+import { motion, useInView, useScroll, useSpring, useTransform } from "framer-motion"
 import { useEffect } from "react"
 import { useRef } from "react"
 
 function App() {
+  const {scrollYProgress} = useScroll()
 
-  const ref = useRef()
+  const ref = useRef();
+  let background = useTransform(scrollYProgress, [0, .5, 1], ['#f21b1b', '#eff21b', '#1bf222'])
 
   const isInview = useInView(ref, {once: true})
 
@@ -14,6 +16,12 @@ function App() {
 
   return (
     <>
+      <motion.div className="scale-0 origin-left w-full h-3 bg-red-500 fixed top-0"
+        style={{
+          scaleX: useSpring(scrollYProgress),
+          background
+        }}
+      ></motion.div>
       <div style={{
         height: '200vh'
       }} className="w-screen">
