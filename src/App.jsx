a49decb0266cf306b7ea11c651a93393ba51f02b
himu <satirtharoy2003@gmail.com>
2024-05-22 15:14:38 +0600

    started useAnimationControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { motion, MotionConfig } from "framer-motion"
 
 function App() {
+  const handleClick = () =>{
 
+  }
   return (
-    <div className="text-5xl">
-      Framer motion started
+    <div className="w-screen h-screen grid place-content-center gap-4">
+      
+      <MotionConfig transition={{
+        duration: .2,
+        ease: 'easeInOut'
+      }}>
+        <motion.button whileHover={{borderRadius: '10px', scale: 1.1}} className="p-4 bg-orange-400 text-white font-semibold">Flip</motion.button>
+      </MotionConfig>
+      <motion.div
+      className="size-28 bg-blue-400"
+      variants={{
+        initial: {
+          rotate: '0deg'
+        },
+        flip: {
+          rotate: '360deg'
+        }
+      }}
+      transition={{
+        duration: .5,
+        ease: 'easeOut'
+      }}
+      whileHover='flip'
+      >
+
+      </motion.div>
     </div>
   )
 }
