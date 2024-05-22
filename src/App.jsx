dcfad43987281eca282599df272c263e43fa117a
himu <satirtharoy2003@gmail.com>
2024-05-22 15:33:59 +0600

    animation controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { motion, MotionConfig } from "framer-motion"
+import { motion, MotionConfig, useAnimationControls } from "framer-motion"
 
 function App() {
+  const controls = useAnimationControls();
   const handleClick = () =>{
-
+    controls.start('flip')
   }
   return (
     <div className="w-screen h-screen grid place-content-center gap-4">
@@ -11,7 +12,7 @@ function App() {
         duration: .2,
         ease: 'easeInOut'
       }}>
-        <motion.button whileHover={{borderRadius: '10px', scale: 1.1}} className="p-4 bg-orange-400 text-white font-semibold">Flip</motion.button>
+        <motion.button onClick={handleClick} whileHover={{borderRadius: '10px', scale: 1.1}} className="p-4 bg-orange-400 text-white font-semibold">Flip</motion.button>
       </MotionConfig>
       <motion.div
       className="size-28 bg-blue-400"
@@ -27,7 +28,8 @@ function App() {
         duration: .5,
         ease: 'easeOut'
       }}
-      whileHover='flip'
+      initial='initial'
+      animate={controls}
       >
 
       </motion.div>
