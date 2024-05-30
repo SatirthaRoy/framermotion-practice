fe96e512740fe5da67f02aeccae5b7ea01190fd1
himu <satirtharoy2003@gmail.com>
2024-05-30 11:58:54 +0600

    from basic to animate presence

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,57 @@
-import { motion, useInView, useScroll, useSpring, useTransform } from "framer-motion"
-import { useEffect } from "react"
-import { useRef } from "react"
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const {scrollYProgress} = useScroll()
+  const [scale, setScale] = useState(1);
+  const [x, setX] = useState(null);
+  const [show, setShow] = useState(true);
 
-  const ref = useRef();
-  let background = useTransform(scrollYProgress, [0, .5, 1], ['#f21b1b', '#eff21b', '#1bf222'])
-
-  const isInview = useInView(ref, {once: true})
-
-  useEffect(() => {
-    console.log('is in view --> ', isInview);
-  }, [isInview])
+  const keyClick = (e) => {
+    console.log(e.key);
+    if (e.key === "ArrowUp") {
+      setScale(scale + 2);
+    }
+    if (e.key === "ArrowDown") {
+      setScale(scale - 3);
+    }
+    if (e.key === "ArrowRight") {
+      setX(x + 100);
+    }
+    if (e.key === "ArrowLeft") {
+      setX(x - 100);
+    }
+    if (e.key === "0") {
+      setShow(!show);
+    }
+  };
 
   return (
-    <>
-      <motion.div className="scale-0 origin-left w-full h-3 bg-red-500 fixed top-0"
-        style={{
-          scaleX: useSpring(scrollYProgress),
-          background
-        }}
-      ></motion.div>
-      <div style={{
-        height: '200vh'
-      }} className="w-screen">
+    <div
+        tabIndex="0"
+        onKeyUpCapture={keyClick}
+        className="grid place-content-center h-screen overflow-hidden"
+      >
+    <AnimatePresence mode="sync">
+        {show && (
+          <motion.div
+            initial={{
+              x: 100,
+            }}
+            animate={{
+              scale: scale,
+              x: x,
+            }}
+            exit={{
+              scale: 0,
+            }}
+            transition={{ type: "spring", duration: 2 }}
+            className="size-14 rounded-lg bg-blue-400"
+          ></motion.div>
+        )}
+    </AnimatePresence>
       </div>
-      <div style={{height: '100vh'}} className="grid place-content-center w-screen">
-        <motion.div className="size-40 bg-green-500 rounded-full"
-        initial={{scale: 1}}
-        whileInView={{
-          scale: 2
-        }}
-        transition={{duration: .5}}
-        ></motion.div>
-      </div>
-      <motion.div ref={ref} className="w-full h-screen bg-red-400"
-        initial={{opacity: 1}}
-        animate={{opacity: isInview ? 0 : 1}}
-        transition={{
-          duration: 1
-        }}
-      ></motion.div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
