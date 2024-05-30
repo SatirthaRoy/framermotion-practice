5da74f261c943fa9d90bceee1200168be1822c1e
himu <satirtharoy2003@gmail.com>
2024-05-30 12:59:38 +0600

    gestures like whileTap, whileHover, whileDrag, whileFocus

--- src/App.jsx
+++ src/App.jsx
@@ -36,20 +36,28 @@ function App() {
         {show && (
           <motion.div
             initial={{
-              x: 0,
+              scale: 0
+            }}
+            // animate={{
+            //   scale: [1, 2, 2, 1.4, 1],
+            //   x: [0, 200, 400, -300, 0],
+            //   rotate: [0, 360, 720, -360, 360],
+            //   borderRadius: ['0%','50%',' 10%', '20%', '0%'],
+            //   backgroundColor: ['#5563e0', '#eb3144', '#d415cd', '#09eb81', '#5563e0']
+            // }}
+            animate= {{
               scale: 1
             }}
-            animate={{
-              scale: [1, 2, 2, 1.4, 1],
-              x: [0, 200, 400, -300, 0],
-              rotate: [0, 360, 720, -360, 360],
-              borderRadius: ['0%','50%',' 10%', '20%', '0%'],
-              backgroundColor: ['#5563e0', '#eb3144', '#d415cd', '#09eb81', '#5563e0']
+            whileHover={{
+              scale: [null, 1.5, 1.4]
+            }}
+            whileTap={{
+              scale: 1
             }}
             exit={{
               scale: 0,
             }}
-            transition={{ ease: 'easeInOut', duration: 4, repeat: Infinity, repeatDelay: 1, times: [0, .2, .5, .7, .85, 1] }}
+            transition={{ ease: 'easeInOut', duration: .3 }}
             className="size-14 rounded-lg bg-blue-400"
           ></motion.div>
         )}
