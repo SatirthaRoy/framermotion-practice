e954ebc4ba34a0d02e9ea2072f24a27d12c9f962
himu <satirtharoy2003@gmail.com>
2024-05-30 12:34:10 +0600

    keyframes animation

--- src/App.jsx
+++ src/App.jsx
@@ -36,16 +36,20 @@ function App() {
         {show && (
           <motion.div
             initial={{
-              x: 100,
+              x: 0,
+              scale: 1
             }}
             animate={{
-              scale: scale,
-              x: x,
+              scale: [1, 2, 2, 1.4, 1],
+              x: [0, 200, 400, -300, 0],
+              rotate: [0, 360, 720, -360, 360],
+              borderRadius: ['0%','50%',' 10%', '20%', '0%'],
+              backgroundColor: ['#5563e0', '#eb3144', '#d415cd', '#09eb81', '#5563e0']
             }}
             exit={{
               scale: 0,
             }}
-            transition={{ type: "spring", duration: 2 }}
+            transition={{ ease: 'easeInOut', duration: 4, repeat: Infinity, repeatDelay: 1, times: [0, .2, .5, .7, .85, 1] }}
             className="size-14 rounded-lg bg-blue-400"
           ></motion.div>
         )}
